ad939bc0ab6b2b5d28516567ae055668c40d892c
shogo.takasaki <stakasaki707@gmail.com>
2024-09-28 22:21:14 +0900

    implemented Topic grid item composable

--- app/src/main/java/jp/shirataki707/cources/MainActivity.kt
+++ app/src/main/java/jp/shirataki707/cources/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import jp.shirataki707.cources.data.Topic
 import jp.shirataki707.cources.ui.theme.CourcesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +28,70 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CourcesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Topic(
+    topic: Topic,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier = modifier) {
+        Row {
+            Image(
+                painter = painterResource(id = topic.icon),
+                contentDescription = stringResource(id = topic.title),
+                modifier = Modifier.size(68.dp)
+            )
+            Column {
+                Text(
+                    text = stringResource(id = topic.title),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp)
+                )
+                CourseCount(
+                    courseCount = topic.courseCount,
+                    modifier = Modifier.padding(start = 16.dp, top = 8.dp)
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CourcesTheme {
-        Greeting("Android")
+fun CourseCount(
+    courseCount: Int,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_grain),
+            contentDescription = null,
+        )
+        Text(
+            text = courseCount.toString(),
+            style = MaterialTheme.typography.labelMedium,
+            modifier = Modifier.padding(start = 8.dp)
+        )
     }
+}
+
+@Preview
+@Composable
+fun CourseCountPreview() {
+    CourseCount(courseCount = 321)
+}
+
+@Preview
+@Composable
+fun TopicPreview() {
+    Topic(
+        topic = Topic(R.string.architecture, 58, R.drawable.architecture)
+    )
 }
