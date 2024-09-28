9c3ca55f43185e0b3f3f52b4f23f362bb0e03f88
shogo.takasaki <stakasaki707@gmail.com>
2024-09-28 22:36:58 +0900

    implemented Topic Grid and CoursesApp

--- app/src/main/java/jp/shirataki707/cources/MainActivity.kt
+++ app/src/main/java/jp/shirataki707/cources/MainActivity.kt
@@ -5,12 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,6 +26,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import jp.shirataki707.cources.data.DataSource
 import jp.shirataki707.cources.data.Topic
 import jp.shirataki707.cources.ui.theme.CourcesTheme
 
@@ -28,7 +36,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CourcesTheme {
+                CoursesApp()
+            }
+        }
+    }
+}
 
+@Composable
+fun CoursesApp() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.padding(8.dp)
+        ) {
+            items(DataSource.topics) { topic ->
+                Topic(topic = topic)
             }
         }
     }
